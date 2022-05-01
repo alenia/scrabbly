b707ad88bafb4b0eec01274dda51d2c8f9903273
Daniela Wellisz <daniela.wellisz@gmail.com>
2022-04-30 21:11:15 -0700

    remove tiles by clicking on them

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import tileBag, { drawTile } from './tileBag';
 
-function Tile( { letter } : { letter : string } ) {
+function Tile( { letter, onClick } : { letter : string, onClick: () => void } ) {
   useEffect(() => {
     console.log(`Adding tile ${letter}`);
     return () => { console.log(`Removing tile ${letter}`); }
   });
   return (
-    <div className="tile">
+    <div className="tile" onClick={onClick}>
       { letter }
     </div>
   );
 }
 
+//function Rack( { tiles } : { tiles: string[] } ) {
+//}
+
 function App() {
   const [tiles, setTiles] = useState([] as string[]);
 
-  const renderTiles = () => tiles.map(t => (<Tile key={t} letter={t[0]} />));
+  const removeTile = (tile : string) => (() => setTiles(tiles.filter((t) => t !== tile)));
+
+  const renderTiles = () => tiles.map(t => (<Tile key={t} letter={t[0]} onClick={removeTile(t)}/>));
   return (
     <div>
       <button onClick = {() => setTiles(drawTile(tiles))}>
