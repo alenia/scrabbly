b4f47ca4bd5b61e4d5f602fe3a85e687837fa1bc
Daniela Wellisz <daniela.wellisz@gmail.com>
2022-07-19 12:40:36 -0700

    allow two players to draw tiles

--- src/App.tsx
+++ src/App.tsx
@@ -29,22 +29,41 @@ function Rack( { player, tiles, discardTile } : { player: number, tiles: string[
 }
 
 function App() {
-  const playerCount = 4;
-  //TODO:
-  //tiles = {
+  //const playerCount = 4;
+  //TODO: racks should be dynamically generated from playerCount. currently it just assumes 2 players.
+  //racks = {
   //  0: [tile, tile, tile],
   //  1: [tile, tile, tile],
   //  2: [tile, tile, tile],
   //  3: [tile, tile]
   //}
-  const [tiles, setTiles] = useState([] as string[]);
-  const [player, setPlayer] = useState(0);
+  const [playerRacks, setPlayerRacks] = useState({0: [], 1: []} as {0: string[], 1: string[]});
+  const [player, setPlayer] = useState(0 as 0 | 1);
+  const allTiles = [...playerRacks[0],...playerRacks[1]];
+  const tiles = playerRacks[player];
+
+  const setTiles = (tiles: string[]) => {
+    if ( player === 0 ) {
+      setPlayerRacks({0: tiles, 1: playerRacks[1] })
+    }
+    if (player === 1 ) {
+      setPlayerRacks({0: playerRacks[0], 1: tiles })
+    }
+  }
+
+  const setNextPlayer = () => {
+    // note playerCount is 2
+    let nextPlayer = (player + 1) % 2 as 0 | 1;
+    console.log(nextPlayer);
+    setPlayer(nextPlayer);
+  }
 
   const drawTile = () => {
-    const nextTile = maybeNewTile(tiles);
+    const nextTile = maybeNewTile(allTiles);
     if(!nextTile) { return }
     setTiles([...tiles, nextTile])
   };
+
   const discardTile = (tile : string) => (() => setTiles(tiles.filter((t) => t !== tile)));
 
   return (
@@ -53,12 +72,12 @@ function App() {
         <button onClick={drawTile}>
           Draw tile
         </button>
-        <button onClick={() => setPlayer((player + 1) % playerCount)}>
+        <button onClick={setNextPlayer}>
           Next player
         </button>
       </div>
-      <Rack player={0} tiles={tiles} discardTile={discardTile}/>
-      <Rack player={1} tiles={tiles} discardTile={discardTile}/>
+      <Rack player={0} tiles={playerRacks[0]} discardTile={discardTile}/>
+      <Rack player={1} tiles={playerRacks[1]} discardTile={discardTile}/>
     </div>
   );
 }
