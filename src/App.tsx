05fdb6e0b90f2c73cd0bb65e110037cdfa2eb371
Daniela Wellisz <daniela.wellisz@gmail.com>
2022-07-19 18:19:15 -0700

    some work pulling out interfaces and helper methods

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,48 @@ import './App.css';
 import tileBag, { maybeNewTile } from './tileBag';
 import Rack from './Rack';
 
+//const playerCount = 4;
+//TODO: is this the right name?
+interface PlayerRacks {
+  0: string[],
+  1: string[]
+}
+
+type PlayerNumber = 0 | 1;
+
+const helpers = {
+  nextPlayerRacks: (tiles: string[], player: PlayerNumber, playerRacks: PlayerRacks) : PlayerRacks => {
+    if (player === 0) {
+      return { 0: tiles, 1: playerRacks[1] }
+    } else { // player === 1
+      return { 0: playerRacks[0], 1: tiles }
+    }
+  },
+  nextPlayer: (player: PlayerNumber) : PlayerNumber => (player + 1) % 2 as PlayerNumber
+}
+
 function App() {
-  //const playerCount = 4;
-  //TODO: racks should be dynamically generated from playerCount. currently it just assumes 2 players.
-  //racks = {
-  //  0: [tile, tile, tile],
-  //  1: [tile, tile, tile],
-  //  2: [tile, tile, tile],
-  //  3: [tile, tile]
-  //}
-  const [playerRacks, setPlayerRacks] = useState({0: [], 1: []} as {0: string[], 1: string[]});
-  const [player, setPlayer] = useState(0 as 0 | 1);
-  const allTiles = [...playerRacks[0],...playerRacks[1]];
-  const tiles = playerRacks[player];
+  const [playerRacks, setPlayerRacks] = useState({0: [], 1: []} as PlayerRacks);
+  const [activePlayer, setActivePlayer] = useState(0 as PlayerNumber);
+  const activeRack = playerRacks[activePlayer];
+
+  const setNextPlayer = () => { setActivePlayer(helpers.nextPlayer(activePlayer)) }
 
   const setTiles = (tiles: string[]) => {
-    if ( player === 0 ) {
-      setPlayerRacks({0: tiles, 1: playerRacks[1] })
-    }
-    if (player === 1 ) {
-      setPlayerRacks({0: playerRacks[0], 1: tiles })
-    }
-  }
-
-  const setNextPlayer = () => {
-    // note playerCount is 2
-    let nextPlayer = (player + 1) % 2 as 0 | 1;
-    setPlayer(nextPlayer);
+    setPlayerRacks(helpers.nextPlayerRacks(tiles, activePlayer, playerRacks))
   }
 
   const drawTile = () => {
+    const allTiles = [...playerRacks[0],...playerRacks[1]];
     const nextTile = maybeNewTile(allTiles);
     if(!nextTile) { return }
-    setTiles([...tiles, nextTile])
+    setTiles([...activeRack, nextTile])
   };
 
-  const discardTile = (tile : string) => (() => setTiles(tiles.filter((t) => t !== tile)));
+  const discardTile = (tile : string) => (() => setTiles(activeRack.filter((t) => t !== tile)));
 
   return (
-    <div className={`currentPlayer${player}`}>
+    <div className={`currentPlayer${activePlayer}`}>
       <div className="buttons">
         <button onClick={drawTile}>
           Draw tile
@@ -50,8 +53,8 @@ function App() {
           Next player
         </button>
       </div>
-      <Rack player={0} tiles={playerRacks[0]} discardTile={discardTile} disabled={player === 0}/>
-      <Rack player={1} tiles={playerRacks[1]} discardTile={discardTile} disabled={player === 1}/>
+      <Rack player={0} tiles={playerRacks[0]} discardTile={discardTile} disabled={activePlayer === 0}/>
+      <Rack player={1} tiles={playerRacks[1]} discardTile={discardTile} disabled={activePlayer === 1}/>
     </div>
   );
 }
